f5943404c0cefe8e013f0e06b5b38e4ecbfa12fe
RusiaYT <127548787+RusiaYT@users.noreply.github.com>
2023-11-17 21:04:10 -0300

    Add files via upload

---
+++ IPWeb.py
@@ -0,0 +1,20 @@
+# Código de: https://github.com/RusiaYT
+from socket import gethostbyname
+import time
+import os
+import sys
+os.system("clear")
+os.system("figlet IpWeb")
+################
+print''' _____________________
+|Creado por: Johnknife|
+|_____________________|'''
+def Miscript():
+	print'        [Bienvenido A IpWeb]'
+	target = raw_input('[~] Ingresa El Sitio: ')
+	targetIP = gethostbyname(target)
+	print '[~] IP De Destino ===>', targetIP
+	print '+------------------------------------+'
+Miscript()
+exit()
+#Johnknife JOD3 4 T0DO5 :)
